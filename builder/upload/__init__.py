f9335c6026dc8068a76f5d55027cfc391f8e3a09
Patrizio Cavarretta <78727572+drypatrick@users.noreply.github.com>
2021-06-24 17:38:54 +0200

    Add port

--- builder/upload/__init__.py
+++ builder/upload/__init__.py
@@ -3,9 +3,10 @@ from pathlib import Path
 from importlib import import_module
 
 
-def run_upload(plugin: str, local: Path, remote: str) -> None:
+def run_upload(plugin: str, port: str, local: Path, remote: str) -> None:
+
     """Load a plugin and start upload."""
     plugin = import_module(f".{plugin}", "builder.upload")
 
     # Run upload
-    plugin.upload(local, remote)
+    plugin.upload(port, local, remote)
