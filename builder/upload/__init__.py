ea6cadc07ee1dbe0961ad9058ae0999d0868a99e
Patrizio Cavarretta <78727572+drypatrick@users.noreply.github.com>
2021-06-24 17:39:17 +0200

    Add lftp

--- builder/upload/__init__.py
+++ builder/upload/__init__.py
@@ -3,10 +3,13 @@ from pathlib import Path
 from importlib import import_module
 
 
-def run_upload(plugin: str, port: str, local: Path, remote: str) -> None:
+def run_upload(plugin: str, port: str, local: Path, remote: str, ftp_host: str, ftp_user: str, ftp_password: str, ftp_remote: str, ftp_mirror_options: str) -> None:
 
     """Load a plugin and start upload."""
     plugin = import_module(f".{plugin}", "builder.upload")
 
     # Run upload
-    plugin.upload(port, local, remote)
+    if plugin == "rsync":
+        plugin.upload(port, local, remote)
+    else:    
+        plugin.upload(local, ftp_host, ftp_user, ftp_password, ftp_remote, ftp_mirror_options)
